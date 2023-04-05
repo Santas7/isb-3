9b0c7f7ca0153f0aff3ca6c731cf28708e4194b4
Santas7 <andreypanyshkin03@gmail.com>
2023-04-05 17:42:58 +0400

    написал простую и понятную реализацию основного меню программы под каждый пункт лабораторной работы

--- main.py
+++ main.py
@@ -17,7 +17,40 @@ def decrypt_data(input_file_path: str, private_key_path: str, symmetric_key_path
 
 
 def menu():
-    pass
+    """
+        основное меню программы
+    """
+    while True:
+        print('Encrypt or decrypt data using SEED algorithm')
+        command = int(input("[1] generate\n[2] encrypt\n[3] decrypt\n--> "))
+
+        if command == 1:
+            os.system("cls")
+            symmetric_key_path = input("symmetric key path--> ")
+            public_key_path = input("public key path--> ")
+            private_key_path = input("private key path--> ")
+            generate_keys(symmetric_key_path, public_key_path, private_key_path)
+            os.system("cls")
+        elif command == 2:
+            os.system("cls")
+            input_file_path = input("input file path--> ")
+            symmetric_key_path = input("symmetric key path--> ")
+            private_key_path = input("private key path--> ")
+            output_file_path = input("output file path--> ")
+            encrypt_data(input_file_path, private_key_path, symmetric_key_path, output_file_path)
+            os.system("cls")
+        elif command == 3:
+            os.system("cls")
+            input_file_path = input("input file path--> ")
+            symmetric_key_path = input("symmetric key path--> ")
+            private_key_path = input("private key path--> ")
+            output_file_path = input("output file path--> ")
+            decrypt_data(input_file_path, private_key_path, symmetric_key_path, output_file_path)
+            os.system("cls")
+        else:
+            os.system("cls")
+            print("Error! I'm not found this command!\n")
+            os.system("cls")
 
 if __name__ == '__main__':
     menu()
