163eb9835a53d70711b547a7199d1857e41b2785
Santas7 <andreypanyshkin03@gmail.com>
2023-04-05 17:39:37 +0400

    создал три основные функции (без реализации) generate_keys, encrypt_data, decrypt_data

--- main.py
+++ main.py
@@ -2,3 +2,15 @@ import os
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import padding, rsa
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+
+
+def generate_keys(symmetric_key_path: str, public_key_path: str, private_key_path: str) -> None:
+    pass
+
+
+def encrypt_data(input_file_path: str, private_key_path: str, symmetric_key_path: str, output_file_path: str):
+    pass
+
+
+def decrypt_data(input_file_path: str, private_key_path: str, symmetric_key_path: str, output_file_path: str):
+    pass
