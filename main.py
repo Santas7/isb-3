38120577239a5fcc7a0d632c988fbd2286fc6017
Santas7 <andreypanyshkin03@gmail.com>
2023-04-05 17:37:57 +0400

    создание основного файла main.py и подключение необходимых библиотек (модулей)

---
+++ main.py
@@ -0,0 +1,4 @@
+import os
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
