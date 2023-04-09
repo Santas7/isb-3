5153e0010b94a8b16a2f85158b48e4e7f611158a
Santas7 <andreypanyshkin03@gmail.com>
2023-04-09 22:42:10 +0400

    добавил класс, пофиксил (исправил) функции, добавил все файлы text.txt - исходный текст, symmetric.txt - зашифрованный ключ, public.pem - публичный ключ, private.pem - приватный ключ, encrypted.txt - зашифрованный текст в ходе работы программы, decrypted.txt - расшифрованный текст

--- main.py
+++ main.py
@@ -1,187 +1,197 @@
 import os
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import padding, rsa
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import serialization, asymmetric, hashes, padding
 import logging
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
+# класс для работы с криптографией
+class Cryptography:
+    def __init__(self, symmetric_key_path: str = 'symmetric.txt', public_key_path: str = 'public.pem', private_key_path: str = 'private.pem') -> None:
+        self.symmetric_key_path = symmetric_key_path
+        self.public_key_path = public_key_path
+        self.private_key_path = private_key_path
 
-def generate_keys(symmetric_key_path: str, public_key_path: str, private_key_path: str) -> None:
-    """
-        1 пункт л.р - Генерация ключей гибридной системы
-        1.1. Сгеренировать ключ для симметричного алгоритма.
-        1.2. Сгенерировать ключи для ассиметричного алгоритма.
-        1.3. Сериализовать ассиметричные ключи.
-        1.4. Зашифровать ключ симметричного шифрования открытым ключом и сохранить по указанному пути.
-        :param symmetric_key_path: путь для ключа симметричного алгоритма
-        :param public_key_path: путь для публичного ключа ассиметричного алгоритма
-        :param private_key_path: путь для закрытого ключа ассиметричного алгоритма
-        :return: None
-    """
-    # Генерация ключа для симметричного алгоритма
-    symmetric_key = os.urandom(16)
-
-    # Генерация ключей для ассиметричного алгоритма
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-    )
-    public_key = private_key.public_key()
-
-    # Сериализация ассиметричных ключей
-    private_key_bytes = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-    public_key_bytes = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    try:
-        with open(public_key_path, 'wb') as f:
-            f.write(public_key_bytes)
-    except Exception as e:
-        logging.error(e)
-    try:
-        with open(private_key_path, 'wb') as f:
-            f.write(private_key_bytes)
-    except Exception as e:
-        logging.error(e)
-
-    # Шифрование ключа симметричного шифрования открытым ключом и сохранение по указанному пути
-    encrypted_symmetric_key = public_key.encrypt(
-        symmetric_key,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
+    def generate_keys(self) -> None:
+        """
+            1 пункт л.р - Генерация ключей гибридной системы
+            1.1. Сгеренировать ключ для симметричного алгоритма.
+            1.2. Сгенерировать ключи для ассиметричного алгоритма.
+            1.3. Сериализовать ассиметричные ключи.
+            1.4. Зашифровать ключ симметричного шифрования открытым ключом и сохранить по указанному пути.
+            :return: None
+        """
+        # Генерация ключа для симметричного алгоритма
+        symmetric_key = os.urandom(16)
+        # Генерация ключей для ассиметричного алгоритма
+        private_key = asymmetric.rsa.generate_private_key(
+            public_exponent=65537,
+            key_size=2048,
         )
-    )
-    try:
-        with open(symmetric_key_path, 'wb') as f:
-            f.write(encrypted_symmetric_key)
-    except Exception as e:
-        logging.error(e)
-
-
-def encrypt_data(input_file_path: str, private_key_path: str, symmetric_key_path: str, output_file_path: str):
-    """
-        2 пункт л.р - Шифрование данных гибридной системой
-        2.1. Расшифровать симметричный ключ.
-        2.2. Зашифровать текст симметричным алгоритмом и сохранить по указанному пути.
-        :param input_file_path: входной файл
-        :param private_key_path: путь для закрытого ключа ассиметричного алгоритма
-        :param symmetric_key_path: путь для ключа симметричного алгоритма
-        :param output_file_path: выходной файл
-        :return: None
-    """
-    # Расшифровка симметричного ключа
-    try:
-        with open(symmetric_key_path, 'rb') as f:
-            encrypted_symmetric_key = f.read()
-    except Exception as e:
-        logging.error(e)
-    try:
-        with open(private_key_path, 'rb') as f:
-            private_key_bytes = f.read()
-    except Exception as e:
-        logging.error(e)
-    private_key = serialization.load_pem_private_key(
-        private_key_bytes,
-        password=None,
-    )
-    symmetric_key = private_key.decrypt(
-        encrypted_symmetric_key,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
+        public_key = private_key.public_key()
+        # Сериализация ассиметричных ключей
+        private_key_bytes = private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
         )
-    )
-
-    # Шифрование текста симметричным алгоритмом и сохранение по указанному пути
-    try:
-        with open(input_file_path, 'rb') as f:
-            data = f.read()
-    except Exception as e:
-        logging.error(e)
-
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.SEED(symmetric_key), modes.CBC(iv))
-    encryptor = cipher.encryptor()
-    try:
-        with open(output_file_path, 'wb') as f:
-            f.write(iv)
-            f.write(encryptor.update(data) + encryptor.finalize())
-    except Exception as e:
-        logging.error(e)
-
-
-def decrypt_data(input_file_path: str, private_key_path: str, symmetric_key_path: str, output_file_path: str):
-    """
-        3 пункт л.р - Дешифрование данных гибридной системой
-        3.1. Расшифровать симметричный ключ.
-        3.2. Расшифровать текст симметричным алгоритмом и сохранить по указанному пути.
-        :param input_file_path: входной файл
-        :param private_key_path: путь для закрытого ключа ассиметричного алгоритма
-        :param symmetric_key_path: путь для ключа симметричного алгоритма
-        :param output_file_path: выходной файл
-        :return: None
-    """
-    # Расшифровка симметричного ключа
-    try:
-        with open(symmetric_key_path, 'rb') as f:
-            encrypted_symmetric_key = f.read()
-    except Exception as e:
-        logging.error(e)
-    try:
-        with open(private_key_path, 'rb') as f:
-            private_key_bytes = f.read()
-    except Exception as e:
-        logging.error(e)
-    private_key = serialization.load_pem_private_key(
-        private_key_bytes,
-        password=None,
-    )
-    symmetric_key = private_key.decrypt(
-        encrypted_symmetric_key,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
+        public_key_bytes = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
         )
-    )
+        try:
+            with open(self.public_key_path, 'wb') as f:
+                f.write(public_key_bytes)
+        except Exception as e:
+            logging.error(e)
+        try:
+            with open(self.private_key_path, 'wb') as f:
+                f.write(private_key_bytes)
+        except Exception as e:
+            logging.error(e)
+        # Шифрование ключа симметричного шифрования открытым ключом и сохранение по указанному пути
+        encrypted_symmetric_key = public_key.encrypt(
+            symmetric_key,
+            asymmetric.padding.OAEP(
+                mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        try:
+            # Запись зашифрованного ключа в файл
+            with open(self.symmetric_key_path, 'wb') as f:
+                f.write(encrypted_symmetric_key)
+        except Exception as e:
+            logging.error(e)
 
-    # Расшифровка текста симметричным алгоритмом и сохранение по указанному пути
-    try:
-        with open(input_file_path, 'rb') as f:
-            data = f.read()
-    except Exception as e:
-        logging.error(e)
-    iv = data[:16]
-    data = data[16:]
-    cipher = Cipher(algorithms.SEED(symmetric_key), modes.CBC(iv))
-    decryptor = cipher.decryptor()
+    def encrypt_data(self, input_file_path: str, output_file_path: str) -> None:
+        """
+            2 пункт л.р - Шифрование данных
+            2.1. Считать зашифрованный ключ симметричного шифрования из файла.
+            2.2. Расшифровать ключ симметричного шифрования закрытым ключом.
+            2.3. Считать данные из файла.
+            2.4. Зашифровать данные симметричным алгоритмом.
+            2.5. Сохранить зашифрованные данные в файл.
+            :param input_file_path: путь к файлу с данными для шифрования
+            :param output_file_path: путь к файлу, в который будут сохранены зашифрованные данные
+            :return: None
+        """
+        # Считывание зашифрованного ключа симметричного шифрования из файла
+        try:
+            with open(self.symmetric_key_path, 'rb') as f:
+                encrypted_symmetric_key = f.read()
+        except Exception as e:
+            logging.error(e)
+        # Расшифровка ключа симметричного шифрования закрытым ключом
+        try:
+            with open(self.private_key_path, 'rb') as f:
+                private_key = serialization.load_pem_private_key(
+                    f.read(),
+                    password=None
+                )
+        except Exception as e:
+            logging.error(e)
+        symmetric_key = private_key.decrypt(
+            encrypted_symmetric_key,
+            asymmetric.padding.OAEP(
+                mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        # Чтение данных из файла
+        try:
+            with open(input_file_path, 'rb') as f:
+                data = f.read()
+        except Exception as e:
+            logging.error(e)
 
-    try:
-        with open(output_file_path, 'wb') as f:
-            f.write(decryptor.update(data) + decryptor.finalize())
-    except Exception as e:
-        logging.error(e)
+        padder = padding.PKCS7(128).padder()
+        padded_text = padder.update(data) + padder.finalize()
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.SEED(symmetric_key), modes.CBC(iv))
+        encryptor = cipher.encryptor()
+        encrypted_data = encryptor.update(padded_text) + encryptor.finalize()
+        encrypted_data = iv + encrypted_data
+        # Сохранение зашифрованных данных в файл
+        try:
+            with open(output_file_path, 'wb') as f:
+                f.write(encrypted_data)
+        except Exception as e:
+            logging.error(e)
 
 
-def menu():
+    def decrypt_data(self, input_file_path: str, output_file_path: str) -> None:
+        """
+            3 пункт л.р - Расшифровка данных
+            3.1. Считать зашифрованный ключ симметричного шифрования из файла.
+            3.2. Расшифровать ключ симметричного шифрования закрытым ключом.
+            3.3. Считать зашифрованные данные из файла.
+            3.4. Расшифровать данные симметричным алгоритмом.
+            3.5. Сохранить расшифрованные данные в файл.
+            :param input_file_path: путь к файлу с зашифрованными данными
+            :param output_file_path: путь к файлу, в который будут сохранены расшифрованные данные
+            :return: None
+        """
+        # Считывание зашифрованного ключа симметричного шифрования из файла
+        try:
+            with open(self.symmetric_key_path, 'rb') as f:
+                encrypted_symmetric_key = f.read()
+        except Exception as e:
+            logging.error(e)
+        # Расшифровка ключа симметричного шифрования закрытым ключом
+        try:
+            with open(self.private_key_path, 'rb') as f:
+                private_key = serialization.load_pem_private_key(
+                    f.read(),
+                    password=None
+                )
+        except Exception as e:
+            logging.error(e)
+        symmetric_key = private_key.decrypt(
+            encrypted_symmetric_key,
+            asymmetric.padding.OAEP(
+                mgf=asymmetric.padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        # Чтение зашифрованных данных из файла
+        try:
+            with open(input_file_path, 'rb') as f:
+                encrypted_data = f.read()
+        except Exception as e:
+            logging.error(e)
+        # Расшифровка данных симметричным алгоритмом
+        iv = encrypted_data[:16]
+        encrypted_data = encrypted_data[16:]
+        cipher = Cipher(algorithms.SEED(symmetric_key), modes.CBC(iv))
+        decryptor = cipher.decryptor()
+        data = decryptor.update(encrypted_data) + decryptor.finalize()
+        unpadder = padding.PKCS7(128).unpadder()
+        data = unpadder.update(data) + unpadder.finalize()
+        # Сохранение расшифрованных данных в файл
+        try:
+            with open(output_file_path, 'wb') as f:
+                f.write(data)
+        except Exception as e:
+            logging.error(e)
+
+if __name__ == '__main__':
     """
-        основное меню программы
-        1 - генерация ключей
-        2 - шифрование данных
-        3 - дешифрование данных
-    """
-    # create main menu with using match/case
-    while True:
+            основное меню программы
+            1 - генерация ключей
+            2 - шифрование данных
+            3 - дешифрование данных
+        """
+    symmetric_key_path = input('Введите путь к файлу для сохранения симметричного ключа: ')
+    private_key_path = input('Введите путь к файлу для сохранения закрытого ключа: ')
+    public_key_path = input('Введите путь к файлу для сохранения открытого ключа: ')
+    object = Cryptography(symmetric_key_path, public_key_path, private_key_path)
+    while 1:
         print('1 - генерация ключей')
         print('2 - шифрование данных')
         print('3 - дешифрование данных')
@@ -189,25 +199,20 @@ def menu():
         choice = input('Выберите пункт меню: ')
         match choice:
             case '1':
-                generate_keys()
+                object.generate_keys()
+                print('Ключи успешно сгенерированы!')
             case '2':
-                encrypt_data(
-                    input_file_path=input('Введите путь к файлу для шифрования: '),
-                    private_key_path=input('Введите путь к закрытому ключу: '),
-                    symmetric_key_path=input('Введите путь к симметричному ключу: '),
-                    output_file_path=input('Введите путь к файлу для сохранения зашифрованных данных: ')
-                )
+                input_file_path = input('Введите путь к файлу для шифрования: ')
+                output_file_path = input('Введите путь к файлу для сохранения зашифрованных данных: ')
+                object.encrypt_data(input_file_path, output_file_path)
+                print('Данные успешно зашифрованы!')
             case '3':
-                decrypt_data(
-                    input_file_path=input('Введите путь к файлу для расшифрования: '),
-                    private_key_path=input('Введите путь к закрытому ключу: '),
-                    symmetric_key_path=input('Введите путь к симметричному ключу: '),
-                    output_file_path=input('Введите путь к файлу для сохранения расшифрованных данных: ')
-                )
+                input_file_path = input('Введите путь к файлу для расшифрования: ')
+                output_file_path = input('Введите путь к файлу для сохранения расшифрованных данных: ')
+                object.decrypt_data(input_file_path, output_file_path)
+                print('Данные успешно расшифрованы!')
             case '4':
+                print('Выход из программы')
                 break
             case _:
                 print('Неверный пункт меню')
-
-if __name__ == '__main__':
-    menu()
